3bb3e1a7aedcd084d3d9d75dda8317b6e1c6c098
Thiago Cardoso de Vasconcelos <107278485+Thiago-Cardoso-de-Vasconcelos@users.noreply.github.com>
2022-10-18 17:57:20 -0300

    main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-// import "./css/index.css"
-import "/src/css/index.css"
+// *****************************
+//  >>> import "/css/index.css" <<<
+// ------------------------------
 
-alert("teste");
+// não consegui fazer funcionar, tive que adicionar o css
+// direto no head <link rel="stylesheet" href="/src/css/index.css">
 
+// *******************************
+
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+
+function setCardType(type){
+  const colors = {
+    visa: ["#2D57F2", "#436D99"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+}
+
+setCardType("mastercard")
