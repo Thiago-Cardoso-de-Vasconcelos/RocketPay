ea320e45d410fcbc3ec50507d45bb08bfe1f51d8
Thiago Cardoso de Vasconcelos <107278485+Thiago-Cardoso-de-Vasconcelos@users.noreply.github.com>
2022-10-18 23:40:18 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 // *****************************
 //  >>> import "/css/index.css" <<<
+//  >>> import IMask from "imask" <<<
 // ------------------------------
-
-// não consegui fazer funcionar, tive que adicionar o css
-// direto no head <link rel="stylesheet" href="/src/css/index.css">
-
+// não consegui fazer Vite funcionar, 
+// tive que adicionar os links direto no index
+// ------------------------------
 // *******************************
-
-
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 
 function setCardType(type){
@@ -20,6 +19,56 @@ function setCardType(type){
   }
   ccBgColor01.setAttribute("fill", colors[type][0])
   ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src",`/public/cc-${type}.svg`)
 }
 
-setCardType("mastercard")
+globalThis.setCardType = setCardType
+
+const securityCode = document.getElementById("security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.getElementById("expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    },
+  },
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.createElement("#card-number")
+const cardNumberPatter = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: //,
+      cardtype: "default",
+    }
+  ],
+  dispatch: function (appended, dynamicMasked){
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+
+  },
+}
