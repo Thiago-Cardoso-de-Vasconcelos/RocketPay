9c39eb31a435252a79a7c7c0a55dfe8938993a90
Thiago Cardoso de Vasconcelos <107278485+Thiago-Cardoso-de-Vasconcelos@users.noreply.github.com>
2022-10-21 01:55:51 -0300

    foi

--- src/main.js
+++ src/main.js
@@ -15,7 +15,9 @@ function setCardType(type){
   const colors = {
     visa: ["#2D57F2", "#436D99"],
     mastercard: ["#DF6F29", "#C69347"],
-    default: ["black", "gray"],
+    elo: ["#000000", "#000000"],
+    hiper: ["#c9bea6", "#9b0000"],
+    default: ["#000000", "gray"],
   }
   ccBgColor01.setAttribute("fill", colors[type][0])
   ccBgColor02.setAttribute("fill", colors[type][1])
@@ -48,8 +50,9 @@ const expirationDatePattern = {
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
 
-const cardNumber = document.createElement("#card-number")
-const cardNumberPatter = {
+
+const cardNumber = document.getElementById("card-number")
+const cardNumberPattern = {
   mask: [
     {
       mask: "0000 0000 0000 0000",
@@ -63,12 +66,72 @@ const cardNumberPatter = {
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: //,
+      regex:
+        /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$/,
+      cardtype: "elo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^606282|^637095|^637599|^637568/,
+      cardtype: "hiper",
+    },
+    {
+      mask: "0000 0000 0000 0000",
       cardtype: "default",
-    }
+    },
   ],
-  dispatch: function (appended, dynamicMasked){
+  dispatch: function (appended, dynamicMasked) {
     const number = (dynamicMasked.value + appended).replace(/\D/g, "")
-
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+    return foundMask
   },
 }
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+ alert("cartão adicionado!")
+})
+
+document.querySelector("form").addEventListener("submit", (event) =>{
+ event.preventDefault()
+ 
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+})
+
+function updateSecurityCode(code){
+  const ccSecutiry = document.querySelector(".cc-security .value")
+  ccSecutiry.innerText = code.length === 0 ? "123" : code
+}
+cardNumberMasked.on("accept", () =>{
+ 
+ const cardType = cardNumberMasked.masked.currentMask.cardtype 
+ setCardType(cardType) 
+ updateCardNumber(cardNumberMasked.value)
+})
+function updateCardNumber(number){
+ const ccNumber = document.querySelector(".cc-number")
+ ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+} 
+
+expirationDateMasked.on("accept", () =>{
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date){
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
+
